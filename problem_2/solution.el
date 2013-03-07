c406763e4775b42e775393d9218f010f82f5d3cd
haled <darren.e.hale@gmail.com>
2013-03-07 10:40:00 -0600

    Initial creation

---
+++ problem_2/solution.el
@@ -0,0 +1,15 @@
+(defun is_even (x)
+  (if (= (mod x 2) 0)
+    x
+    0
+  )
+)
+
+(defun gen_fib (first second max)
+  (setq x (+ first second))
+  (if (< x max)
+    (cons x (gen_fib second x (- max 1)))
+  )
+)
+
+(apply '+ (mapcar 'is_even (gen_fib 1 1 4000000)))
